3739ad6926b60fd9b3ae3090602fd921e8b9ee07
Khanh Duong Quoc <dqkqdlot@gmail.com>
2023-10-08 07:38:19 +0900

    feat: export with different name for each api call

---
+++ src/api/index.ts
@@ -0,0 +1,8 @@
+import { _getQuestions, _getUsers, _saveQuestion, _saveQuestionAnswer } from "./_DATA";
+
+export {
+  _getQuestions as getQuestions,
+  _getUsers as getUsers,
+  _saveQuestion as saveQuestion,
+  _saveQuestionAnswer as saveQuestionAnswer,
+};
