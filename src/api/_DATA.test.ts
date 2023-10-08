bbbce2ab913b4bd28f20cdb5de144f0c5fb991b5
Khanh Duong Quoc <dqkqdlot@gmail.com>
2023-10-09 05:06:17 +0900

    refactor: migrate to vite

--- src/api/_DATA.test.ts
+++ src/api/_DATA.test.ts
@@ -1,6 +1,13 @@
-import type { QuestionsDictionary, UsersDictionary } from "../interfaces";
-import type { AnswerId } from "./../interfaces/index";
-import { _getQuestions, _getUsers, _saveQuestion, _saveQuestionAnswer } from "./_DATA";
+import type { QuestionsDictionary, UsersDictionary } from "../interfaces"
+import type { AnswerId } from "./../interfaces/index"
+import {
+  _getQuestions,
+  _getUsers,
+  _saveQuestion,
+  _saveQuestionAnswer,
+} from "./_DATA"
+
+import { describe, expect, it, vi } from "vitest"
 
 describe("Test save questions", () => {
   it("success", async () => {
@@ -9,13 +16,13 @@ describe("Test save questions", () => {
         optionOneText: "option1",
         optionTwoText: "option2",
         author: "author1",
-      })
+      }),
     ).resolves.toMatchObject({
       author: "author1",
       optionOne: { votes: [], text: "option1" },
       optionTwo: { votes: [], text: "option2" },
-    });
-  });
+    })
+  })
 
   describe("empty, null or undefined arguments", () => {
     test.each([
@@ -36,45 +43,53 @@ describe("Test save questions", () => {
             optionOneText,
             optionTwoText,
             author,
-          })
-        ).rejects.toEqual("Please provide optionOneText, optionTwoText, and author");
-      }
-    );
-  });
-});
+          }),
+        ).rejects.toEqual(
+          "Please provide optionOneText, optionTwoText, and author",
+        )
+      },
+    )
+  })
+})
 
 describe("Test save question's answers", () => {
-  let users: UsersDictionary;
-  let questions: QuestionsDictionary;
+  let users: UsersDictionary
+  let questions: QuestionsDictionary
 
   beforeAll(async () => {
-    users = await _getUsers();
-    questions = await _getQuestions();
-  });
+    users = await _getUsers()
+    questions = await _getQuestions()
+  })
 
   describe("success", () => {
-    test.each(["optionOne", "optionTwo"])("answerId is '%s'", async (answerId) => {
-      await expect(
-        _saveQuestionAnswer({
-          authedUser: Object.values(users)[0].id,
-          qid: Object.values(questions)[0].id,
-          answerId: answerId as AnswerId,
-        })
-      ).resolves.toBe(true);
-    });
-  });
+    test.each(["optionOne", "optionTwo"])(
+      "answerId is '%s'",
+      async (answerId) => {
+        await expect(
+          _saveQuestionAnswer({
+            authedUser: Object.values(users)[0].id,
+            qid: Object.values(questions)[0].id,
+            answerId: answerId as AnswerId,
+          }),
+        ).resolves.toBe(true)
+      },
+    )
+  })
 
   describe("failed", () => {
     describe("arguments should not be empty, null or undefined", () => {
-      test.each(["", null, undefined])("authedUser is '%s'", async (authedUser) => {
-        await expect(
-          _saveQuestionAnswer({
-            authedUser,
-            qid: Object.values(questions)[0].id,
-            answerId: "optionOne",
-          })
-        ).rejects.toEqual("Please provide authedUser, qid, and answer");
-      });
+      test.each(["", null, undefined])(
+        "authedUser is '%s'",
+        async (authedUser) => {
+          await expect(
+            _saveQuestionAnswer({
+              authedUser,
+              qid: Object.values(questions)[0].id,
+              answerId: "optionOne",
+            }),
+          ).rejects.toEqual("Please provide authedUser, qid, and answer")
+        },
+      )
 
       test.each(["", null, undefined])("qid is '%s'", async (qid) => {
         await expect(
@@ -82,9 +97,9 @@ describe("Test save question's answers", () => {
             authedUser: Object.values(users)[0].id,
             qid,
             answerId: "optionOne",
-          })
-        ).rejects.toEqual("Please provide authedUser, qid, and answer");
-      });
+          }),
+        ).rejects.toEqual("Please provide authedUser, qid, and answer")
+      })
 
       test.each(["", null, undefined])("answerId is '%s'", async (answerId) => {
         await expect(
@@ -92,65 +107,65 @@ describe("Test save question's answers", () => {
             authedUser: Object.values(users)[0].id,
             qid: Object.values(questions)[0].id,
             answerId: answerId as AnswerId,
-          })
-        ).rejects.toEqual("Please provide authedUser, qid, and answer");
-      });
-    });
+          }),
+        ).rejects.toEqual("Please provide authedUser, qid, and answer")
+      })
+    })
 
     describe("valid arguments but do not exist", () => {
       it("answerId should be optionOne or optionTwo", async () => {
-        jest.useFakeTimers();
-        expect.assertions(1);
+        vi.useFakeTimers()
+        expect.assertions(1)
 
         try {
           _saveQuestionAnswer({
             authedUser: Object.values(users)[0].id,
             qid: Object.values(questions)[0].id,
             answerId: "optionThree" as AnswerId,
-          });
+          })
 
-          jest.runAllTimers();
+          vi.runAllTimers()
         } catch (err) {
           // eslint-disable-next-line jest/no-conditional-expect
-          expect(err).toBeInstanceOf(TypeError);
+          expect(err).toBeInstanceOf(TypeError)
         }
-      });
+      })
 
       it("user should exist in database", () => {
-        jest.useFakeTimers();
-        expect.assertions(1);
+        vi.useFakeTimers()
+        expect.assertions(1)
 
         try {
           _saveQuestionAnswer({
             authedUser: "123",
             qid: Object.values(questions)[0].id,
             answerId: "optionThree" as AnswerId,
-          });
+          })
 
-          jest.runAllTimers();
+          vi.runAllTimers()
         } catch (err) {
           // eslint-disable-next-line jest/no-conditional-expect
-          expect(err).toBeInstanceOf(TypeError);
+          expect(err).toBeInstanceOf(TypeError)
         }
-      });
+      })
 
       it("question id should exist in database", () => {
-        jest.useFakeTimers();
-        expect.assertions(1);
+        vi.useFakeTimers()
+        expect.assertions(1)
 
         try {
           _saveQuestionAnswer({
             authedUser: Object.values(users)[0].id,
             qid: "123",
             answerId: "optionThree" as AnswerId,
-          });
+          })
 
-          jest.runAllTimers();
+          vi.runAllTimers()
         } catch (err) {
           // eslint-disable-next-line jest/no-conditional-expect
-          expect(err).toBeInstanceOf(TypeError);
+          expect(err).toBeInstanceOf(TypeError)
         }
-      });
-    });
-  });
-});
+      })
+    })
+  })
+})
--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,26 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import { store } from "./app/store";
-import { fetchUsers } from "./features/users/usersSlice";
-import "./index.css";
-import reportWebVitals from "./reportWebVitals";
-import { routesConfig } from "./routes";
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { Provider } from "react-redux"
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
+import { store } from "./app/store"
+import { fetchUsers } from "./features/users/usersSlice"
+import "./index.css"
+import { routesConfig } from "./routes"
 
-const router = createBrowserRouter(routesConfig);
+const router = createBrowserRouter(routesConfig)
 
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 
 const start = async () => {
-  await store.dispatch(fetchUsers());
+  await store.dispatch(fetchUsers())
 
   root.render(
     <React.StrictMode>
       <Provider store={store}>
         <RouterProvider router={router} />
       </Provider>
-    </React.StrictMode>
-  );
-};
+    </React.StrictMode>,
+  )
+}
 
-start();
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+start()
