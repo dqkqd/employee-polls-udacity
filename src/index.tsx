2cc4bc102af336cfeec3123c1e169afd93030dba
Khanh Duong Quoc <dqkqdlot@gmail.com>
2023-10-08 09:02:33 +0900

    feat: add router to index and initlal load users

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import App from "./App";
 import { store } from "./app/store";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
-root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
-);
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import LoginPage from "./features/auth/LoginPage";
+import { fetchUsers } from "./features/users/usersSlice";
 
+const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+
+const start = async () => {
+  await store.dispatch(fetchUsers());
+
+  root.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
+    </React.StrictMode>
+  );
+};
+
+start();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
