1d24b9535b5336ce00e7787e69e7ccce3f7e300d
Khanh Duong Quoc <dqkqdlot@gmail.com>
2023-10-08 15:26:49 +0900

    feat: handle submit login button

--- src/features/auth/LoginForm.tsx
+++ src/features/auth/LoginForm.tsx
@@ -1,37 +1,52 @@
 import { Visibility, VisibilityOff } from "@mui/icons-material";
 import { Button, FormControl, IconButton, Input, InputAdornment, InputLabel } from "@mui/material";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAppDispatch } from "../../app/hook";
+import { validateUser } from "./authSlice";
 
 const LoginForm = () => {
-  const [userId, setUserId] = useState("");
-  const [userPassword, setUserPassword] = useState("");
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
 
   const handleUserId = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setUserId(e.target.value);
+    setId(e.target.value);
   };
 
-  const handleUserPassword = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setUserPassword(e.target.value);
+  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
   };
 
   const handleClickShowPassword = () => setShowPassword(!showPassword);
   const handleMouseDownShowPassword = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
   };
+
+  const handleLogin = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    const user = await dispatch(validateUser({ id, password })).unwrap();
+    if (user) {
+      navigate("/");
+    }
+  };
+
   return (
     <>
       <FormControl>
         <InputLabel htmlFor="user-id">Employee ID</InputLabel>
-        <Input id="user-id" value={userId} onChange={handleUserId} />
+        <Input id="user-id" value={id} onChange={handleUserId} />
       </FormControl>
       <FormControl>
         <InputLabel htmlFor="password">Password</InputLabel>
         <Input
           id="password"
           type={showPassword ? "text" : "password"}
-          value={userPassword}
-          onChange={handleUserPassword}
+          value={password}
+          onChange={handlePasswordChange}
           endAdornment={
             <InputAdornment position="end">
               <IconButton
@@ -44,7 +59,9 @@ const LoginForm = () => {
           }
         />
       </FormControl>
-      <Button variant="contained">Log in</Button>
+      <Button variant="contained" onClick={handleLogin} disabled={!id || !password}>
+        Log In
+      </Button>
     </>
   );
 };
--- src/features/auth/authSlice.ts
+++ src/features/auth/authSlice.ts
@@ -1,6 +1,12 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import type { RootState } from "../../app/store";
 import type { AuthedUser } from "../../interfaces";
+import { selectUserById } from "../users/usersSlice";
+
+interface ValidatingUser {
+  id: string;
+  password: string;
+}
 
 const initialState: AuthedUser = {
   id: null,
@@ -8,10 +14,29 @@ const initialState: AuthedUser = {
   password: null,
 };
 
+export const validateUser = createAsyncThunk(
+  "auth/validate",
+  async ({ id, password }: ValidatingUser, { getState }) => {
+    const state = getState();
+    const user = selectUserById(state as RootState, id);
+    if (user && user.password === password) {
+      return user;
+    }
+    return undefined;
+  }
+);
+
 const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(validateUser.fulfilled, (state, action) => {
+      if (action.payload) {
+        return action.payload;
+      }
+    });
+  },
 });
 
 export const selectAuthedUser = (state: RootState) => state.auth;
