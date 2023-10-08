928aea905bd9549b6ebd6aa3a02213b3ae8d6c9b
Khanh Duong Quoc <dqkqdlot@gmail.com>
2023-10-08 19:48:05 +0900

    feat: setup type for testing redux

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,28 @@
-import { configureStore, type Action, type ThunkAction } from "@reduxjs/toolkit";
+import {
+  combineReducers,
+  configureStore,
+  type Action,
+  type PreloadedState,
+  type ThunkAction,
+} from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice";
 import usersReducer from "../features/users/usersSlice";
 
+// Create the root reducer independently to obtain the RootState type
+const rootReducer = combineReducers({
+  auth: authReducer,
+  users: usersReducer,
+});
+
+export function setupStore(preloadedState?: PreloadedState<RootState>) {
+  return configureStore({
+    reducer: {
+      auth: authReducer,
+      users: usersReducer,
+    },
+    preloadedState,
+  });
+}
 export const store = configureStore({
   reducer: {
     auth: authReducer,
@@ -9,8 +30,9 @@ export const store = configureStore({
   },
 });
 
+export type AppStore = ReturnType<typeof setupStore>;
 export type AppDispatch = typeof store.dispatch;
-export type RootState = ReturnType<typeof store.getState>;
+export type RootState = ReturnType<typeof rootReducer>;
 export type AppThunk<ReturnType = void> = ThunkAction<
   ReturnType,
   RootState,
--- src/utils/test-utils.tsx
+++ src/utils/test-utils.tsx
@@ -1,11 +1,17 @@
+import type { PreloadedState } from "@reduxjs/toolkit";
 import { configureStore } from "@reduxjs/toolkit";
-import { render } from "@testing-library/react";
+import { render, type RenderOptions } from "@testing-library/react";
 import React, { type PropsWithChildren } from "react";
 import { Provider } from "react-redux";
+import type { AppStore, RootState } from "../app/store";
 import authSlice from "../features/auth/authSlice";
 import usersSlice from "../features/users/usersSlice";
 
 // As a basic setup, import your same slice reducers
+interface ExtendedRenderOptions extends Omit<RenderOptions, "queries"> {
+  preloadedState?: PreloadedState<RootState>;
+  store?: AppStore;
+}
 
 export function renderWithProviders(
   ui: React.ReactElement,
@@ -17,9 +23,10 @@ export function renderWithProviders(
         auth: authSlice,
         users: usersSlice,
       },
+      preloadedState,
     }),
     ...renderOptions
-  } = {}
+  }: ExtendedRenderOptions = {}
 ) {
   function Wrapper({ children }: PropsWithChildren<unknown>): JSX.Element {
     return <Provider store={store}>{children}</Provider>;
