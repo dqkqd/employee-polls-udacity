02f3fba556e552b8788befa7d3866c4497cf5043
Khanh Duong Quoc <dqkqdlot@gmail.com>
2023-10-09 22:29:11 +0900

    feat: wrap LoginPage inside BaseAuthPage

--- src/features/auth/LoginPage.tsx
+++ src/features/auth/LoginPage.tsx
@@ -1,28 +1,11 @@
-import { Box, Stack, Typography } from "@mui/material"
+import BaseAuthPage from "./BaseAuthPage"
 import LoginForm from "./LoginForm"
 
 const LoginPage = () => {
   return (
-    <Box justifyContent="center" display="flex" mt={10}>
-      <Stack
-        sx={{ width: "300px" }}
-        spacing={2}
-        component="form"
-        style={{ textAlign: "center" }}
-      >
-        <Typography variant="h4" mt={2} fontWeight="bolder">
-          Employee Polls
-        </Typography>
-
-        <img
-          src="login-logo.jpg"
-          alt="login logo"
-          style={{ borderRadius: "100%" }}
-        />
-
-        <LoginForm />
-      </Stack>
-    </Box>
+    <BaseAuthPage>
+      <LoginForm />
+    </BaseAuthPage>
   )
 }
 
