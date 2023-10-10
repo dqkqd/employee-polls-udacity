2274170834a153e45b82b0ad0ac63c8fc2dc1487
Khanh Duong Quoc <dqkqdlot@gmail.com>
2023-10-10 22:54:43 +0900

    feat: add signup user thunk and actions

--- src/features/auth/authSlice.ts
+++ src/features/auth/authSlice.ts
@@ -1,4 +1,5 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
+import { saveUser } from "../../api"
 import type { RootState } from "../../app/store"
 import type { AuthedUser } from "../../interfaces"
 import { selectUserById } from "../users/usersSlice"
@@ -6,6 +7,13 @@ import { selectUserById } from "../users/usersSlice"
 interface ValidatingUser {
   id: string
   password: string
+}
+
+interface RegisteringUser {
+  id: string
+  name: string
+  password: string
+  avatarURL?: string
 }
 
 const initialState: AuthedUser = {
@@ -27,6 +35,14 @@ export const validateUser = createAsyncThunk(
   },
 )
 
+export const signupUser = createAsyncThunk(
+  "auth/signup",
+  async (user: RegisteringUser, { getState }) => {
+    const result = await saveUser(user)
+    return result
+  },
+)
+
 const authSlice = createSlice({
   name: "auth",
   initialState,
@@ -41,6 +57,17 @@ const authSlice = createSlice({
         return { id, name, password, status: "success" }
       })
       .addCase(validateUser.rejected, (state) => {
+        state.status = "failed"
+      })
+      .addCase(signupUser.pending, (state) => {
+        state.status = "loading"
+      })
+      .addCase(signupUser.fulfilled, (state, action) => {
+        // TODO(khanhdq) add user to store
+        const { id, name, password } = action.payload
+        return { id, name, password, status: "success" }
+      })
+      .addCase(signupUser.rejected, (state) => {
         state.status = "failed"
       })
   },
