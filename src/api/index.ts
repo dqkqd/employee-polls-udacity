0dc508cef0890d0028b67cb7dbe89d74ca46c2ad
Khanh Duong Quoc <dqkqdlot@gmail.com>
2023-10-10 22:15:32 +0900

    feat: public saveUser

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,6 +3,7 @@ import {
   _getUsers,
   _saveQuestion,
   _saveQuestionAnswer,
+  _saveUser,
 } from "./_DATA"
 
 export {
@@ -10,4 +11,5 @@ export {
   _getUsers as getUsers,
   _saveQuestion as saveQuestion,
   _saveQuestionAnswer as saveQuestionAnswer,
+  _saveUser as saveUser,
 }
