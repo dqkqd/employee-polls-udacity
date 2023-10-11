cee41adac753b3d9ce0ea35d26f04600dfbc6ff5
Khanh Duong Quoc <dqkqdlot@gmail.com>
2023-10-12 00:02:12 +0900

    feat: add more test users and setting up users

--- src/utils/test-data.ts
+++ src/utils/test-data.ts
@@ -1,13 +1,29 @@
+import { saveUser } from "../api"
+
 export const initialUsers = {
-  ids: ["@fake-user-1"],
+  ids: ["@fake-user-1", "@fake-user-2"],
   entities: {
     "@fake-user-1": {
       id: "@fake-user-1",
       name: "Fake User 1",
       password: "@fake-user-1-password",
-      avatarURL: null,
+      avatarURL: "https://picsum.photos/200",
+      questions: [],
+      answers: {},
+    },
+    "@fake-user-2": {
+      id: "@fake-user-2",
+      name: "Fake User 2",
+      password: "@fake-user-2-password",
+      avatarURL: "https://picsum.photos/200",
       questions: [],
       answers: {},
     },
   },
+}
+
+export const setUpTestUsers = async (users = initialUsers) => {
+  for (const user of Object.values(users.entities)) {
+    await saveUser(user)
+  }
 }
