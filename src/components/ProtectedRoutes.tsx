08f4d25769ae049a48bf66e5e8d8b2097add3063
Khanh Duong Quoc <dqkqdlot@gmail.com>
2023-10-12 22:35:06 +0900

    refactor: remove ProtectedRoutes, move tests to Dashboard

--- src/components/ProtectedRoutes.tsx
+++
@@ -1,9 +0,0 @@
-import { Outlet } from "react-router-dom"
-import { useAuth } from "../app/hook"
-
-const ProtectedRoutes = () => {
-  useAuth()
-  return <Outlet />
-}
-
-export default ProtectedRoutes
