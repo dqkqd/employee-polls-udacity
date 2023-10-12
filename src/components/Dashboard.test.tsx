5448cb759acdbfd0d74b3bb162115999003d507e
Khanh Duong Quoc <dqkqdlot@gmail.com>
2023-10-12 22:41:44 +0900

    feat: add tests for not navigating authorized user

--- src/components/Dashboard.test.tsx
+++ src/components/Dashboard.test.tsx
@@ -1,8 +1,21 @@
 import { screen } from "@testing-library/react"
 import { it } from "vitest"
+import { initialAuth, initialUsers } from "../utils/test-data"
 import { renderDefault } from "../utils/test-utils"
 
-it("User will be redirected to /login if unauthorized", () => {
-  renderDefault({ route: "/" })
-  expect(screen.getByLabelText("log-in-page-title")).toHaveTextContent("Log In")
+describe("Authorization", () => {
+  it("User will be redirected to /login if unauthorized", () => {
+    renderDefault({ route: "/home" })
+    expect(screen.getByLabelText("log-in-page-title")).toHaveTextContent(
+      "Log In",
+    )
+  })
+
+  it("User will not be redirected to /login if authorized", () => {
+    renderDefault({
+      preloadedState: { users: initialUsers, auth: initialAuth },
+      route: "/home",
+    })
+    expect(screen.getByText(`Hello ${initialAuth.name}`)).toBeInTheDocument()
+  })
 })
--- src/utils/test-data.ts
+++ src/utils/test-data.ts
@@ -1,4 +1,5 @@
 import { saveUser } from "../api"
+import { AuthedUser } from "../interfaces"
 
 export const initialUsers = {
   ids: ["@fake-user-1", "@fake-user-2"],
@@ -20,6 +21,14 @@ export const initialUsers = {
       answers: {},
     },
   },
+}
+
+export const initialAuth: AuthedUser = {
+  id: "@fake-user-1",
+  name: "Fake User 1",
+  password: "@fake-user-1-password",
+  avatarURL: "https://picsum.photos/200",
+  status: "success",
 }
 
 export const setUpTestUsers = async (users = initialUsers) => {
