0ad0cae1ca233afb49e9a9400aa1c3ef6c4bc329
Khanh Duong Quoc <dqkqdlot@gmail.com>
2023-10-15 08:44:01 +0900

    feat: add test for moving to question detail

--- src/features/questions/QuestionCard.test.tsx
+++ src/features/questions/QuestionCard.test.tsx
@@ -1,28 +1,49 @@
-import { screen } from "@testing-library/react"
+import { act, screen, waitFor } from "@testing-library/react"
 import { QuestionNotFoundError } from "../../errors"
-import { initialQuestions } from "../../utils/test-data"
-import { renderWithNoRoutes } from "../../utils/test-utils"
+import {
+  initialAuth,
+  initialQuestions,
+  initialUsers,
+} from "../../utils/test-data"
+import { renderDefault, renderWithNoRoutes } from "../../utils/test-utils"
 import QuestionCard from "./QuestionCard"
 
-describe("Test question card", () => {
-  it("render", async () => {
-    const question = Object.values(initialQuestions.entities)[0]
-    if (!question) {
-      throw new QuestionNotFoundError(question)
-    }
+it("Render", async () => {
+  const question = Object.values(initialQuestions.entities)[0]
+  if (!question) {
+    throw new QuestionNotFoundError(question)
+  }
 
-    renderWithNoRoutes(<QuestionCard id={question.id} />)
+  renderWithNoRoutes(<QuestionCard id={question.id} />)
 
-    expect(
-      screen.getByRole("heading", {
-        name: question.author,
-      }),
-    ).toBeInTheDocument()
+  expect(
+    screen.getByRole("heading", {
+      name: question.author,
+    }),
+  ).toBeInTheDocument()
 
-    const createdDate = new Date(question.timestamp).toUTCString()
-    expect(screen.getByText(createdDate)).toBeInTheDocument()
-    expect(screen.getByRole("button", { name: "Show" })).toBeInTheDocument()
+  const createdDate = new Date(question.timestamp).toUTCString()
+  expect(screen.getByText(createdDate)).toBeInTheDocument()
+  expect(screen.getByRole("button", { name: "Show" })).toBeInTheDocument()
+})
+
+it("Click button should move to question details", async () => {
+  const { user } = renderDefault({
+    route: "/home",
+    preloadedState: {
+      users: initialUsers,
+      questions: initialQuestions,
+      auth: initialAuth,
+    },
   })
 
-  it.todo("click show button should move to question details")
+  const buttons = screen.getAllByRole("button", { name: "Show" })
+
+  expect(screen.queryByText(/poll by/i)).not.toBeInTheDocument()
+  await act(async () => {
+    user.click(buttons[0])
+  })
+  await waitFor(() => {
+    expect(screen.getByText(/poll by/i)).toBeInTheDocument()
+  })
 })
