54e5fca065aa1007a11f545b2d04d6c09e4ead03
Khanh Duong Quoc <dqkqdlot@gmail.com>
2023-10-15 01:01:03 +0900

    feat: remove unneeded tests in UserLoginSelection

--- src/features/users/UserLoginSelection.test.tsx
+++ src/features/users/UserLoginSelection.test.tsx
@@ -1,27 +1,9 @@
-import { act, screen, waitFor } from "@testing-library/react"
+import { screen } from "@testing-library/react"
 import { expect, it } from "vitest"
-import { initialUsers } from "../../utils/test-data"
-import { renderDefault } from "../../utils/test-utils"
+import { renderWithNoRoutes } from "../../utils/test-utils"
+import UserLoginSelection from "./UserLoginSelection"
 
-it("Test login by selecting user", async () => {
-  const { user } = renderDefault({ route: "/login/users" })
-
+it("Render", async () => {
+  renderWithNoRoutes(<UserLoginSelection />)
   expect(screen.getByText("Select employee to login")).toBeInTheDocument()
-
-  const avatars = screen.getAllByLabelText("user-login-avatar")
-  expect(avatars).toHaveLength(initialUsers.ids.length)
-  avatars.forEach((avatar) => expect(avatar).toBeInTheDocument())
-
-  const names = screen.getAllByText(/^fake user/i)
-  expect(names).toHaveLength(initialUsers.ids.length)
-  expect(names[0]).toHaveTextContent("Fake User 1")
-  expect(names[1]).toHaveTextContent("Fake User 2")
-
-  await act(async () => {
-    await user.click(names[0])
-  })
-
-  await waitFor(() => {
-    expect(screen.getByText("Hello Fake User 1")).toBeInTheDocument()
-  })
 })
