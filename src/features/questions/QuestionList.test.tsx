52c3f1b4b8f0272262d6f61515a622f10b4e50e0
Khanh Duong Quoc <dqkqdlot@gmail.com>
2023-10-15 09:15:08 +0900

    feat: add QuestionList in homepage

---
+++ src/features/questions/QuestionList.test.tsx
@@ -0,0 +1,71 @@
+import { act, screen } from "@testing-library/react"
+import { Question } from "../../interfaces"
+import { addedQuestions, initialQuestions } from "../../utils/test-data"
+import { renderWithNoRoutes } from "../../utils/test-utils"
+import QuestionList from "./QuestionList"
+
+it("Render", () => {
+  const questions = addedQuestions
+
+  if (questions.length > 6) {
+    throw new Error("too many questions to be render on the same page")
+  }
+
+  renderWithNoRoutes(
+    <QuestionList title="My list" ids={questions.map((q) => q.id)} />,
+  )
+
+  expect(
+    screen.getByRole("heading", { level: 5, name: "My list" }),
+  ).toBeInTheDocument()
+
+  for (const question of questions) {
+    const questionCardsTitle = screen.getAllByRole("heading", {
+      level: 5,
+      name: question.author,
+    })
+    expect(questionCardsTitle.length > 0).toBeTruthy()
+  }
+})
+
+it("Pagination", async () => {
+  const questions = Object.values(initialQuestions.entities).filter(
+    (q): q is Question => q !== undefined,
+  )
+  const authors = questions.map((q) => q.author)
+
+  if (questions.length <= 6) {
+    throw new Error("need more questions to test pagination")
+  }
+
+  const { user } = renderWithNoRoutes(
+    <QuestionList title="My list" ids={questions.map((q) => q.id)} />,
+  )
+
+  const textContents = screen
+    .getAllByRole("heading", {
+      level: 5,
+    })
+    .map((heading) => heading.textContent)
+
+  // the first one must be title, the next 6 ones are cards title
+  expect(textContents).toHaveLength(7)
+  expect(textContents[0]).toBe("My list")
+  expect(authors).toEqual(expect.arrayContaining(textContents.slice(1)))
+
+  // move to next page
+  await act(async () => {
+    await user.click(screen.getByRole("button", { name: "Go to page 2" }))
+  })
+  expect(screen.getByRole("button", { name: "page 2" })).toBeInTheDocument()
+
+  const textContentsPage2 = screen
+    .getAllByRole("heading", {
+      level: 5,
+    })
+    .map((heading) => heading.textContent)
+
+  expect(textContentsPage2.length).toBeTruthy()
+  expect(textContentsPage2[0]).toBe("My list")
+  expect(authors).toEqual(expect.arrayContaining(textContentsPage2.slice(1)))
+})
---
+++ src/features/questions/QuestionList.tsx
@@ -0,0 +1,51 @@
+import { Box, Pagination, Paper, Stack, Typography } from "@mui/material"
+import { useState } from "react"
+import { QuestionId } from "../../interfaces"
+import QuestionCard from "./QuestionCard"
+
+const QuestionList = (props: { title: string; ids: QuestionId[] }) => {
+  const [page, setPage] = useState(1)
+  const handleChangePage = (
+    event: React.ChangeEvent<unknown>,
+    page: number,
+  ) => {
+    setPage(page)
+  }
+
+  const perPage = 6
+  const totalPages = Math.ceil(props.ids.length / perPage)
+  const selectedIds = props.ids.slice(perPage * (page - 1), perPage * page)
+
+  const questionCards = selectedIds.map((id) => (
+    <QuestionCard key={id} id={id} />
+  ))
+
+  return (
+    <Box display="flex" alignItems="center" justifyContent="center" my={5}>
+      <Paper elevation={5} sx={{ width: "80%" }}>
+        <Stack display="flex" alignItems="center" justifyContent="center">
+          <Typography variant="h5" textAlign="center" fontWeight="bold" my={2}>
+            {props.title}
+          </Typography>
+          <Box
+            display="flex"
+            flexWrap="wrap"
+            justifyContent="center"
+            maxHeight={600}
+          >
+            {questionCards}
+          </Box>
+
+          <Pagination
+            count={totalPages}
+            onChange={handleChangePage}
+            size="large"
+            sx={{ marginY: 2 }}
+          />
+        </Stack>
+      </Paper>
+    </Box>
+  )
+}
+
+export default QuestionList
